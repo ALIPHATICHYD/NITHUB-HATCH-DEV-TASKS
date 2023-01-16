55aa78237f96f61536a773ab68e3abc3ee5c1c15
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-01-16 18:10:24 +0100

    Add files via upload

---
+++ WEEK 12/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import SideBar from './sidebar';
+
+function App() {
+  return (
+   <div className='flex '>
+    <SideBar/>
+   
+   </div>
+  );
+}
+
+export default App;
---
+++ WEEK 12/src/SideBar.js
@@ -0,0 +1,14 @@
+const SideBar = () => {
+    return (
+        <div className="">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    )
+
+};
+
+export default SideBar;
