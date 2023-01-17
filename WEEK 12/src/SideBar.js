8c96161b3b9998998578bb56972ca64d19016326
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-01-17 15:01:55 +0100

    Update SideBar.js

--- WEEK 12/src/SideBar.js
+++ WEEK 12/src/SideBar.js
@@ -1,6 +1,6 @@
 const SideBar = () => {
     return (
-        <div className="">
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg ">
             <i>A</i>
             <i>B</i>
             <i>C</i>
