8ad000eed4c2fe77972ed4e35d2fc731ffcbea95
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-05 22:23:53 +0100

    Add files via upload

---
+++ WEEK-15/tag.js
@@ -0,0 +1,20 @@
+let price = 100;
+
+let cost = price;
+
+price = price + 1;
+101;
+console.log(cost);
+
+let hieght = 90;
+
+let width = hieght++;
+
+let depth = hieght;
+
+let richKid = 1000;
+let godAbeg = ++richKid;
+
+console.log(godAbeg);
+
+console.log(richKid);
