ce32712bbf3ce5c945bb5d20ffdf35d9ef735c96
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-18 13:45:17 +0100

    Update lastwedtask.js

--- WEEK-16/lastwedtask.js
+++ WEEK-16/lastwedtask.js
@@ -1,5 +1,17 @@
 /** 
 In JavaScript, there are two ways to access object properties: dot notation and bracket notation. 
 Both notations allow you to access an object's properties, but they differ in their syntax and functionality.
-Dot notation is the most common way to access an object property in JavaScript. It involves using a dot (.) to separate the object name from the property name. For example:
+Dot notation is the most common way to access an object property in JavaScript.
+It involves using a dot (.) to separate the object name from the property name.
+For example:
 **/
+
+const myObject = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+console.log(myObject.name); // Output: "John"
+console.log(myObject.age); // Output: 30
+console.log(myObject.city); // Output: "New York"
