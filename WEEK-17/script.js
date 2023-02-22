1787d6d9fb0dd3076cc021d1cabd99819574fa9a
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-22 17:53:12 +0100

    Update script.js

--- WEEK-17/script.js
+++ WEEK-17/script.js
@@ -4,3 +4,4 @@
 // pop; this is used to remove the last element in an array...
 // unshift; adds element to the beginning of the array
 // push; thi is used to add element to the end of the array
+// shift; this removes element at the beginning of an array
