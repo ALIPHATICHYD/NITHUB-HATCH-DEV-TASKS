6522ecdfcd318c11e4321e898702e619225faa6c
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-22 18:07:40 +0100

    Update script.js

--- WEEK-17/script.js
+++ WEEK-17/script.js
@@ -5,3 +5,14 @@
 // unshift; adds element to the beginning of the array
 // push; thi is used to add element to the end of the array
 // shift; this removes element at the beginning of an array
+
+
+const example = [];
+example.unshift(90);
+console.log(example);
+example.unshift(102);
+console.log(example);
+example.unshift(-1);
+console.log(example);
+example.unshift("Bala Blue");
+console.log(example);
