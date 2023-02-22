e1e3b6cd934c49520a9ae181512ea26760d05038
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-22 11:22:37 +0100

    Update JS-COURSE.js

--- WEEK-14/JS-COURSE.js
+++ WEEK-14/JS-COURSE.js
@@ -361,13 +361,7 @@ Code Output
     var removedFromOurArray = ourArray.shift();  // removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", "cat"]
     console.log(ourArray);
      
- // Manipulate Arrays With Unshift() 
-    ourArray.unshift("Happy"); // ourArray now equals ["Happy", "J", "cat"]     
-    ourArray.shift(); // ourArray now equals ["J", "cat"]
-    ourArray.unshift("Happy"); // ourArray now equals ["Happy", "J", "cat"]     
-    console.log(ourArray);
 
-// Shopping List      
         
         
         
