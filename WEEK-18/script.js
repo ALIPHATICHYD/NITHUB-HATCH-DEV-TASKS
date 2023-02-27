29125300306a3f5efa6d9b2ef8b59acd35c5cfff
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-27 11:16:45 +0100

    Add files via upload

---
+++ WEEK-18/script.js
@@ -0,0 +1,16 @@
+        /** FUNCTIONS
+         * Function allows us to create reusable codes in JavaScript
+         * We have the word function
+         * Then we have the function name
+         * Theres always parenthesis there, you can pass information into the parenthesis 
+         * Then theres the curly bracket, opening and closing 
+         * Everything inside the curly  brackets is run anytime the function is called or invoked 
+         * So anytime the function is called its says Shege, Banza!!! in the console 
+         */
+
+function ourReusableFunction() {
+    console.log("Shege, Banza!!!");
+}
+ourReusableFunction();
+
+// Passing Values to 
