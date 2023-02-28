3830ea54b865c654fb18198031726ae64533fc85
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-28 01:26:01 +0100

    Contd from JS-COURSE

--- WEEK-18/script.js
+++ WEEK-18/script.js
@@ -50,3 +50,19 @@ function fun1() {
 // Assign 5 oopsGlobal here 
     oopsGlobal = 5;
 }
+
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += "oopsGlobal: " + oopsGlobal;
+    }
+
+    console.log(output)
+          
+}
+fun1();
+fun2();
