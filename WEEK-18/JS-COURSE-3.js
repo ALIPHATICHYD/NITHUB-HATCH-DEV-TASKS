9d933f6e82a888e352cf5f934b4fabf1afc9362f
ALIPHATIC <adegokefuad1@gmail.com>
2023-03-06 13:50:46 +0100

    Update on Chaining If Else Statements

--- WEEK-18/JS-COURSE-3.js
+++ WEEK-18/JS-COURSE-3.js
@@ -18,5 +18,32 @@ console.log(orderMyLogic(7));
 
 
 /** Chaining If Else Statements
+ * You can also chain If and Else If Statements 
+ * So we are going to do a small challenge here 
+ * Write Chained if/else if statements to fulfil the following conditions;
  * 
+ * num < 5 - return "Tiny";
+ * num < 10 - return "Small";
+ * num < 15 - return "Medium";
+ * num < 20 - return "Large";
+ * num >= 20 - return "Huge"; 
  */
+
+function testSize(num){
+    // Only change code below this line
+    if (num < 5){
+        return "Tiny";
+    } else if (num < 10){
+        return "Small";
+    } else if (num < 15){
+        return "Medium";
+    }else if (num < 20){
+        return "Large";
+    } else {
+        return "Huge";
+    }
+
+    return "Change Me";
+}
+// You can change the value here to test 
+console.log(testSize(7));
