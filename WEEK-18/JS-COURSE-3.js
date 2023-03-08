f9ca1dc6cf1c2b2ee46e1e4c3519c585585f7003
ALIPHATIC <adegokefuad1@gmail.com>
2023-03-08 05:14:23 +0100

    Still on Switch Statements

--- WEEK-18/JS-COURSE-3.js
+++ WEEK-18/JS-COURSE-3.js
@@ -193,9 +193,81 @@ console.log(switchOfStuff(2));
 
 function sequentialSizes(val){
     var answer = "";
+    switch(val){
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;         
 
+    }
     return answer;
 }
 
 // change this value to test 
 console.log(sequentialSizes(1));
+
+
+/** Replacing If Else Chains with Switch 
+ * Sometimes the switch statements can be easier to write and understand than a chain of if else statements.
+ * So we are going to change these chain of if else statements to switch statements.
+ * We will start with the switch keyword.
+ */
+
+function chainToSwitch(val){
+    var answer = ""
+    // Only change code below this line 
+
+    if (val === "bob"){
+        answer = "Marley";
+    } else if (val === 42){
+        answer = "The Answer";
+    } else if (val === 1){
+        answer = "There is no #1";
+    } else if (bal === 99){
+        answer = "Missed me by this much!";
+    } else if (val === 7){
+        answer = "Ate Nine";
+    }
+
+// And now we have changed the chain of If Else Statements to chain of Switch Statements.
+
+    switch(val){
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    
+
+    // Only change code above this line 
+    return answer; 
+}
+
+console.log(chainToSwitch());
+
+
+/** Returning Boolean Values from Functions
+ * 
+ */
