667c5a8afcfaa1e8531b64631c8134ffefac47f7
ALIPHATIC <adegokefuad1@gmail.com>
2023-03-19 01:13:41 +0100

    Using Objects for Lookups

--- WEEK-21/JS-COURSE-4.js
+++ WEEK-21/JS-COURSE-4.js
@@ -1,4 +1,37 @@
 /** Using Object for Lookups
- * 
+ * Objects can be thought of as a key value storage like a dictionary.
+ * You can use object to look up values.
+ * So in this case we have switch statements that returns certain values.
+ * So when you pass in 'alpha' to the function it returns "Adams", if you pass in 'bravo' it returns 'Boston'.
  * 
  */
+
+function phoneticLookup(val) {
+    var result = "";
+
+
+// Only change code below this line
+    switch(val){
+        case "alpha":
+            result = "Adams";
+            break;
+        case "bravo":
+            result = "Boston";
+            break;
+        case "charlie":
+            result = "Chicago";
+            break;
+        case "delta":
+            result = "Denver";
+            break;
+        case "echo":
+            result = "Easy";
+            break;
+        case "foxtrot":
+            result = "Frank";
+            break;
+        
+    }
+
+
+}
