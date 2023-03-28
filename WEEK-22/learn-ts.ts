2c06ab83cfc98297bd2d91159882f08b98dc260b
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-03-28 10:20:21 +0000

    adding this week ts

---
+++ WEEK-22/learn-ts.ts
@@ -0,0 +1,26 @@
+class BluePrint {
+
+    constructor(
+        buildingLen: string, 
+        buildingHieght: string,
+        buildingWidth: string,
+        buildingFoundationType: string,
+    ){
+        this.length = buildingLen;
+        this.height = buildingHieght;
+        this.width = buildingWidth;
+        this.foundationType = buildingFoundationType;
+    }
+
+
+
+    length: string;
+    width: string;
+    height: string;
+    foundationType: string;
+    isCompleted: boolean;
+
+
+
+}
+
