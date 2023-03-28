85cc86ef983030605606daaa461711fad6e368b5
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-03-28 14:34:38 +0000

    Syntax and tools

--- WEEK-22/learn-ts.ts
+++ WEEK-22/learn-ts.ts
@@ -254,3 +254,60 @@ const object = backpack.get();
 backpack.add(23);
 
 // Argument of type 'number' is not assignable to parameter of type 'string'.
+
+/** Structural Type System
+One of TypeScript’s core principles is that type checking focuses on the shape that values have. This is sometimes called “duck typing” or “structural typing”.
+In a structural type system, if two objects have the same shape, they are considered to be of the same type.
+*/
+
+interface Point {
+  x: number;
+  y: number;
+}
+ 
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}`);
+}
+ 
+// logs "12, 26"
+const point = { x: 12, y: 26 };
+logPoint(point);
+
+/** 
+The point variable is never declared to be a Point type. However, TypeScript compares the shape of point to the shape of Point in the type-check. They have the same shape, so the code passes.
+The shape-matching only requires a subset of the object’s fields to match.
+*/
+const point3 = { x: 12, y: 26, z: 89 };
+logPoint(point3); // logs "12, 26"
+ 
+const rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect); // logs "33, 3"
+ 
+const color = { hex: "#187ABF" };
+logPoint(color);
+
+// Argument of type '{ hex: string; }' is not assignable to parameter of type 'Point'.
+// Type '{ hex: string; }' is missing the following properties from type 'Point': x, y
+
+
+// There is no difference between how classes and objects conform to shapes:
+
+class VirtualPoint {
+  x: number;
+  y: number;
+ 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+ 
+const newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint); // logs "13, 56"
+
+// If the object or class has all the required properties, TypeScript will say they match, regardless of the implementation details.
+
+
+
+
+
