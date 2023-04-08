bed1b456e26f6bf4a57646e8cb211195f92234fd
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-08 09:39:17 +0100

    Generating Random Numbers

--- WEEK-22/JS-COURSE-5.js
+++ WEEK-22/JS-COURSE-5.js
@@ -1,5 +1,9 @@
 /** Generate Random Whole Numbers 
  * Often you want a random whole number  instead of a decimal whole number
+ * This can be accomplished with the Math.floor 
+ * So when we pass in (Math.random * 20), and then we round down to the nearest whole number. 
+ * This is going to create a whole number between 0 and 19.
+ * Remember Math.random can never be 1. It can be 0 but can never be quite 1
  */
 
 var randomNumberBetween0and19 = Math.floor(Math.random * 20)
