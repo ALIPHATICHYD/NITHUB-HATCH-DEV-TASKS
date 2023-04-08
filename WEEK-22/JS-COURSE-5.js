6a6eb93365775886d40f2224d3fd0bd918c80995
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-08 09:04:16 +0100

    Random Whole Numbers

---
+++ WEEK-22/JS-COURSE-5.js
@@ -0,0 +1,12 @@
+/** Generate Random Whole Numbers 
+ * Often you want a random whole number  instead of a decimal whole number
+ */
+
+var randomNumberBetween0and19 = Math.floor(Math.random * 20)
+
+function randomWholeNum(){
+
+    return Math.random();
+}
+
+console.log(randomWholeNum());
