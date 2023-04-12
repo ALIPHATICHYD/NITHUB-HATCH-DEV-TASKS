d93105fbbda71835e23bfff47c283f6994b06f1e
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-12 18:00:10 +0100

    Examples of OOP

--- WEEK-24/revisionOOP.ts
+++ WEEK-24/revisionOOP.ts
@@ -1,12 +1,16 @@
 class Staff {
     private name: string;
     private email: string;
+    private educationalQualification: String;
+    private salary: ;
 
 
 
     constructor(name: string, email: string){
         this.name = name;
         this.email = email;
+        this.educationalQualification: String;
+        this.salary = "10,000$"
     }
 }
 
@@ -29,4 +33,18 @@ class TeachingStaff extends Staff{
     private faculty: string;
     private department: string;
     private courseTaking: string[];
+
+    constructor(name: string, email: string, faculty: string, department: string, courseTaking: string[]){
+        super(name, email);
+        this.faculty = faculty;
+        this.department = department;
+        this.courseTaking = courseTaking;
+    }
+
 }
+
+const BossMike = new NonTeachingStaff("Mike", "mike@gmail.com", ["Security", "Food Vendor"])
+const Jumo = new TeachingStaff("Jumoke", "Garba", "Arts", "Sociology", ["Chemistry", "Advanced Maths"])
+
+console.log(BossMike);
+console.log(Jumo);
