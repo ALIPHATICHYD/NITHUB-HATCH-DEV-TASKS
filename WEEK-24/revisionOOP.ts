06534d27b4745f8f2088a0d2975d9d4be78ed44c
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-12 17:44:45 +0100

    Example of OOP

--- WEEK-24/revisionOOP.ts
+++ WEEK-24/revisionOOP.ts
@@ -1,1 +1,30 @@
-class Stad 
+class Staff {
+    private name: string;
+    private email: string;
+
+
+
+    constructor(name: string, email: string){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+
+// Non-teaching and Teaching Staff
+
+class NonTeachingStaff extends Staff {
+    private role: string;
+    constructor(name: string, email: string, role: string){
+        super(name, email);
+        this.role = role;
+    }
+}
+
+class TeachingStaff extends Staff {
+    private subject: string;
+    constructor(name: string, email: string, subject: string){
+        super(name, email);
+        this.subject = subject;
+    }
+}
