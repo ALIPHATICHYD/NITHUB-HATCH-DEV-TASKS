b286c188a5eff3dec7e9092e431ec3ac397e5490
Adegoke Fuad Ajibola <210407063@live.unilag.edu.ng>
2023-04-15 11:07:28 +0100

    Update class.ts
    More Explanations

--- WEEK-24/class.ts
+++ WEEK-24/class.ts
@@ -17,5 +17,37 @@ class Shapes {
         this.area = result;
     }
 
+    get areaOfShape(): number{
+        return this.area;
+    }
+    
+    calculateArea(){
+        return this.area * 2;
+    }
 
 }
+
+ /** Access Modifiers
+ Access modifiers are used to control the access of the properties and methods of a class.
+ There are three access modifiers in TypeScript:
+ 1. public: Public properties and methods can be accessed from anywhere.
+ 2. private: Private properties and methods can only be accessed from within the class.
+ 3. protected: Protected properties and methods can only be accessed from within the class and its subclasses.
+ 
+
+Instance Methods & Static Methods
+Instance methods are methods that can be called on an instance of a class.
+Static methods are methods that can be called on a class itself.
+
+A static property is a property that is tied to a class
+A static method is a method that is tied to a class
+A static method can be called without instantiating the class
+A static method can only access static properties
+*/
+    
+
+
+const sqr = new Shapes("square");
+
+
+
