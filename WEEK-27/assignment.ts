e90b3a6fe6f284aabfb0bf4cde2d895b709d52d0
Adegoke Fuad Ajibola <adegokefuad1@gmail.com>
2023-05-05 14:31:44 +0000

    Explaining Typescript Types

---
+++ WEEK-27/assignment.ts
@@ -0,0 +1,16 @@
+/** TypeScript Types
+* string: Represents a string value.
+* number: Represents a numeric value.
+* boolean: Represents a boolean value (true or false).
+* any: Represents any type of value. This is the default type if no type is specified.
+* void: Represents a function that does not return a value.
+* null: Represents a null value.
+* undefined: Represents an undefined value.
+* object: Represents any non-primitive value, i.e., any value that is not a string, number, boolean, null, or undefined.
+* Array<T>: Represents an array of values of type T.
+* Tuple: Represents an array of fixed length, where each element can have a different type.
+* Enum: Represents a set of named constants.
+* Union Types: Represents a type that can be one of several types.
+* Intersection Types: Represents a type that has properties and methods from multiple types.
+* Type Aliases: Allows you to create a new name for a type.
+ */
