bb1e52e8e87362c02cc71dd9023f271b46a94dcc
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-16 00:59:36 +0100

    Example 3 of the Leetcode Problem

--- WEEK-33/plus-one.ts
+++ WEEK-33/plus-one.ts
@@ -23,4 +23,14 @@
  * Explanation: The array represents the integer 4321.
  * Incrementing by one gives 4321 + 1 = 4322.
  * Thus, the result should be [4,3,2,2].
+
+
+
+ * Example 3:
+ * Input: digits = [9]
+ * Output: [1,0]
+ * Explanation: The array represents the integer 9.
+ * Incrementing by one gives 9 + 1 = 10.
+ * Thus, the result should be [1,0].
+
  */
