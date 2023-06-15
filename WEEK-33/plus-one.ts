2c05ecc203527fc4ca8fe05adb3404ed4a447075
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-16 00:56:46 +0100

    Example 1 of the Plus One problem

--- WEEK-33/plus-one.ts
+++ WEEK-33/plus-one.ts
@@ -4,4 +4,13 @@
  * The large integer does not contain any leading 0's.
  * 
  * Increment the large integer by one and return the resulting array of digits.
+ * 
+ * 
+ * 
+ * Example 1
+ * Input: digits = [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * Incrementing by one gives 123 + 1 = 124.
+ * Thus, the result should be [1,2,4].
  */
