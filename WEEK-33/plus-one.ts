a0f5a2069dc9c57467823288772b611a96139e78
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-16 00:55:15 +0100

    Plus one Leetcode Problem

---
+++ WEEK-33/plus-one.ts
@@ -0,0 +1,7 @@
+/** PLUS ONE LEET CODE PROBLEM
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+ * The digits are ordered from most significant to least significant in left-to-right order. 
+ * The large integer does not contain any leading 0's.
+ * 
+ * Increment the large integer by one and return the resulting array of digits.
+ */
