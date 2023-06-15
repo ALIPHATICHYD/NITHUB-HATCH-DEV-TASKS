d19554621e25447cae4eae77af1eb22a3d5e8fb5
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-16 00:58:16 +0100

    Example 2 of the Leetcode Problem

--- WEEK-33/plus-one.ts
+++ WEEK-33/plus-one.ts
@@ -4,13 +4,23 @@
  * The large integer does not contain any leading 0's.
  * 
  * Increment the large integer by one and return the resulting array of digits.
- * 
- * 
- * 
+ 
+
+
+
  * Example 1
  * Input: digits = [1,2,3]
  * Output: [1,2,4]
  * Explanation: The array represents the integer 123.
  * Incrementing by one gives 123 + 1 = 124.
  * Thus, the result should be [1,2,4].
+ 
+
+
+ * Example 2:
+ * Input: digits = [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * Incrementing by one gives 4321 + 1 = 4322.
+ * Thus, the result should be [4,3,2,2].
  */
