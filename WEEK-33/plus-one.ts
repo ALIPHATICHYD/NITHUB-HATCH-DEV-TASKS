03df361f6bf39a873786adc7db079c7d3c70a263
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-16 01:30:19 +0100

    Plus One Leet-Code Solution

--- WEEK-33/plus-one.ts
+++ WEEK-33/plus-one.ts
@@ -42,6 +42,15 @@
 
 
 function plusOne(digits: number[]): number[] {
-
-
+    let carry = 1;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (carry === 0) break;
+        const sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = Math.floor(sum / 10);
+    }
+    if (carry > 0) {
+        digits.unshift(carry);
+    }
+    return digits;
 };
