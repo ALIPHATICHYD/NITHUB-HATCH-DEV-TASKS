df482d8904122366a0a310a2dfd78ea3ffbd3a8d
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-17 23:40:34 +0100

    Console.log

--- WEEK-33/plus-one.ts
+++ WEEK-33/plus-one.ts
@@ -54,3 +54,17 @@ function plusOne(digits: number[]): number[] {
     }
     return digits;
 };
+
+console.log(plusOne([1, 2, 3]));
+
+console.log(plusOne([4, 3, 2, 1]));
+
+console.log(plusOne([9]));
+
+console.log(plusOne([9, 9, 9, 9]));
+
+console.log(plusOne([1, 9, 9, 9]));
+
+console.log(plusOne([1, 9, 9, 8]));
+
+console.log(plusOne([1, 9, 9, 0]));
