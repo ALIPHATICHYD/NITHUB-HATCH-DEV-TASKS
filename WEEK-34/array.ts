01f3caf84a4c5bd5db33e71530592de913d02e71
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-20 14:35:19 +0100

    Leetcode DSA

---
+++ WEEK-34/array.ts
@@ -0,0 +1,9 @@
+/** * Implement a dynamic array in your chosen language with the following operations
+* Add(val)
+* An element to the end of the array. 
+* Copy and double if necessary.
+* Get(i) - returns the element at index i, throw exception if out of bounds.
+* Set(i, val) - sets the value at index i to val. throw exception if out of bounds.
+* Size() - returns the size of the array
+* Capacity() - returns the capacity of the array
+*/ 
