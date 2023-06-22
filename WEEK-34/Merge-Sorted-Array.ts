21e76427ddd6e3508d9d32471cddfcc693099384
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-22 02:37:28 +0100

    Solution to the Merge Sorted Array DSA

--- WEEK-34/Merge-Sorted-Array.ts
+++ WEEK-34/Merge-Sorted-Array.ts
@@ -44,3 +44,23 @@ You can easily solve this problem if you simply think about two elements at a ti
 Hint 2  
 If you simply consider one element each at a time from the two arrays and make a decision and proceed accordingly, you will arrive at the optimal solution.
  */
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1;
+    let j = n - 1;
+    let k = nums1.length - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+
+};
