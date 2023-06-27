2869c95299d390eb880e4bd3377466e469711e6d
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-28 00:02:58 +0100

    Generating the Text and image

--- Javascript LaunchPad/index.js
+++ Javascript LaunchPad/index.js
@@ -13,3 +13,8 @@ let favoritePlace = "Hub";
 
 // 4. Configure the AI by setting a temperature from 0 to 1.
 // The higher temperature, the more random & experimental output.
+
+let temperature = 0.6;
+
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
