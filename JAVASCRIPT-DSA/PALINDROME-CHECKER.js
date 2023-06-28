b5d8642d14d4b1db46a4041f9096838501465fd9
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-29 00:28:56 +0100

    Adding Test Cases

--- JAVASCRIPT-DSA/PALINDROME-CHECKER.js
+++ JAVASCRIPT-DSA/PALINDROME-CHECKER.js
@@ -8,4 +8,21 @@ Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces
 We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+
+        Test Cases
+
+palindrome("eye") should return a boolean.
+palindrome("eye") should return true.
+palindrome("_eye") should return true.
+palindrome("race car") should return true.
+palindrome("not a palindrome") should return false.
+palindrome("A man, a plan, a canal. Panama") should return true.
+palindrome("never odd or even") should return true.
+palindrome("nope") should return false.
+palindrome("almostomla") should return false.
+palindrome("My age is 0, 0 si ega ym.") should return true.
+palindrome("1 eye for of 1 eye.") should return false.
+palindrome("0_0 (: /-\ :) 0-0") should return true.
+palindrome("five|\_/|four") should return false.      
  */
