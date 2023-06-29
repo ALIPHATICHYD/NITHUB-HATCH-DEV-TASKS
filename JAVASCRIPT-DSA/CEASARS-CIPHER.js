30d079d678ab6dc8ce7b691438e23187de8c91d5
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-29 14:36:06 +0100

    Caesers Cipher DSA Question

---
+++ JAVASCRIPT-DSA/CEASARS-CIPHER.js
@@ -0,0 +1,16 @@
+/* Caesars Cipher
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+Test Cases
+
+rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+rot13("SERR YBIR?") should decode to the string FREE LOVE?
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+*/
