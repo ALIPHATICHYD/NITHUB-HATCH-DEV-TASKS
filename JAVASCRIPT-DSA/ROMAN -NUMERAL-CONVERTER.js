271c17ee547fc5367c77d75c870878d1a391c20f
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-29 14:26:57 +0100

    Adding Test Cases

--- JAVASCRIPT-DSA/ROMAN -NUMERAL-CONVERTER.js
+++ JAVASCRIPT-DSA/ROMAN -NUMERAL-CONVERTER.js
@@ -18,4 +18,30 @@ I   -	1
 All roman numerals answers should be provided in upper-case.
 
 
+Test Cases
+
+convertToRoman(2) should return the string II.
+convertToRoman(3) should return the string III.
+convertToRoman(4) should return the string IV.
+convertToRoman(5) should return the string V.
+convertToRoman(9) should return the string IX.
+convertToRoman(12) should return the string XII.
+convertToRoman(16) should return the string XVI.
+convertToRoman(29) should return the string XXIX.
+convertToRoman(44) should return the string XLIV.
+convertToRoman(45) should return the string XLV.
+convertToRoman(68) should return the string LXVIII
+convertToRoman(83) should return the string LXXXIII
+convertToRoman(97) should return the string XCVII
+convertToRoman(99) should return the string XCIX
+convertToRoman(400) should return the string CD
+convertToRoman(500) should return the string D
+convertToRoman(501) should return the string DI
+convertToRoman(649) should return the string DCXLIX
+convertToRoman(798) should return the string DCCXCVIII
+convertToRoman(891) should return the string DCCCXCI
+convertToRoman(1000) should return the string M
+convertToRoman(1004) should return the string MIV
+convertToRoman(1006) should return the string MVI
+convertToRoman(1023) should return the string MXXIII
 */
