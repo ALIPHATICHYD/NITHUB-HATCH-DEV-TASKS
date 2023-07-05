1b004af34af8f9d25c584795f7729dd640bd4017
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-05 19:37:06 +0100

    Roman Numeral Converter DSA Solution

--- JAVASCRIPT-DSA/ROMAN -NUMERAL-CONVERTER.js
+++ JAVASCRIPT-DSA/ROMAN -NUMERAL-CONVERTER.js
@@ -47,6 +47,22 @@ convertToRoman(1023) should return the string MXXIII
 */
 
 function convertToRoman(num) {
+    let roman = "";
+    let romanNumeral = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC:90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV:4,
+        I: 1
+    };
     return num;
    }
    
