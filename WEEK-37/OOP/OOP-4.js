420d01de965eba581a6f30e0f9247d60c4c8d54a
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-07 23:59:23 +0100

    Solving the task

--- WEEK-37/OOP/OOP-4.js
+++ WEEK-37/OOP/OOP-4.js
@@ -20,3 +20,11 @@ Tests
 dog.sayLegs() should return the given string.
 Your code should use the this keyword to access the numLegs property of dog.
 */
+
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
+  };
+  
+  dog.sayLegs();
