9db36770df5b964d7770f3b29c61bc8c37741055
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-07 23:46:49 +0100

    Tests

--- WEEK-37/OOP/OOP-2.js
+++ WEEK-37/OOP/OOP-2.js
@@ -9,4 +9,8 @@ console.log(duck.name);
 Dot notation is used on the object name, duck, followed by the name of the property, name, to access the value of Aflac.
 
 Print both properties of the dog object to your console.
+
+Tests
+Your code should use console.log to print the value for the name property of the dog object.
+Your code should use console.log to print the value for the numLegs property of the dog object.
 */
