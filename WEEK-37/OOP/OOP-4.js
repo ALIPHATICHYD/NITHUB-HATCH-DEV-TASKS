73d9412ed569de36ca76fc41fd8fc08a92227415
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-08 00:00:40 +0100

    Solution to The OOP 4

--- WEEK-37/OOP/OOP-4.js
+++ WEEK-37/OOP/OOP-4.js
@@ -24,7 +24,7 @@ Your code should use the this keyword to access the numLegs property of dog.
 let dog = {
     name: "Spot",
     numLegs: 4,
-    sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
   };
   
   dog.sayLegs();
