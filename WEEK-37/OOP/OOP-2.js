26809a2b148eb6e9b5e9495ca905a1555809fc8d
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-07 23:49:44 +0100

    Solution to OOP Task-2

--- WEEK-37/OOP/OOP-2.js
+++ WEEK-37/OOP/OOP-2.js
@@ -14,3 +14,12 @@ Tests
 Your code should use console.log to print the value for the name property of the dog object.
 Your code should use console.log to print the value for the numLegs property of the dog object.
 */
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+  };
+  // Only change code below this line
+
+  console.log(dog.name)
+  console.log(dog.numLegs)
